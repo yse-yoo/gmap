96f3cdf98fef06235e64244158fb6669d0c63615
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-12-16 11:44:45 +0900

    add markers

--- simple/index.js
+++ simple/index.js
@@ -1,22 +1,73 @@
 var map;
+var marks = {}
+const zoom = 15
+var centerPosition = { lat: 35.46598110000001, lng: 139.622062 };
+
+const c = 'A'.charCodeAt(0);
+const alphabets = Array.apply(null, new Array(26)).map((v, i) => {
+    return String.fromCharCode(c + i)
+})
 
 function initMap() {
-    var position = { lat: -35.397, lng: 150.644 };
-    var zoom = 3
     map = new google.maps.Map(document.getElementById("map"), {
-        center: position,
+        center: centerPosition,
         zoom: zoom,
     });
-
-    addMarker(position, map);
+    addMarker(centerPosition, map);
+    addCircle(centerPosition)
 }
 
-function addMarker(location, map) {
+function addMarker(location) {
     new google.maps.Marker({
         position: location,
-        label: 'A',
+        label: 'X',
         map: map,
     });
+}
+function addCircle(location) {
+    var circleOpts = {
+        center: location,
+        map: map,
+        radius: 1000,
+        strokeColor: 'rgba(255, 0, 0, 0.5)',
+        fillColor: 'rgba(255, 0, 0, 0.1)',
+    }
+    new google.maps.Circle(circleOpts);
+}
+
+function searchAddress() {
+    var address = document.getElementById('address').value
+    if (!address) return
+
+    const geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ address: address }, function (results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            var placeId = results[0].place_id
+            marks[placeId] = results[0]
+            drawMarker()
+        } else {
+            console.error('Geocode was not successful for the following reason: ' + status)
+        }
+    })
+}
+
+function drawMarker() {
+    console.log(marks)
+    var index = 0
+    for (id in marks) {
+        var place = marks[id]
+        var latlng = {
+            lat: place.geometry.location.lat(),
+            lng: place.geometry.location.lng()
+        }
+        new google.maps.Marker({
+            position: latlng,
+            map: map,
+            label: alphabets[index]
+        })
+        index++
+    }
+    map.setCenter(latlng)
 }
 
 window.onload = initMap;
